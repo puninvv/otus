2d190883c29253d1f7e484d0058397d0d67601e1
punin <punin@speechpro.com>
2020-10-28 20:21:25 +0300

    Создать минимальный сервис, который 1) отвечает на порту 8000 2) имеет http-метод GET /health/ RESPONSE: {"status": "OK"}

---
+++ k8s concepts. part2/service/HealthyService/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using HealthyService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<HealthStatus> GetStatus()
+        {
+            return new HealthStatus()
+            {
+                Status = Status.OK,
+            };
+        }
+    }
+}
---
+++ k8s concepts. part2/service/HealthyService/Models/HealthStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HealthyService.Models
+{
+    public enum Status
+    { 
+        Unknown,
+        OK,
+    }
+
+    public class HealthStatus
+    {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Status Status { get; set; }
+    }
+}
